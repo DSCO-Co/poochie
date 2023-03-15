f5a4af469d15b0caf1dc2c02eeed89c191c59b1a
Callam Ingram <callam@berkeley.edu>
2023-03-15 14:55:16 -0700

    imported the tailwind footer and synced it with pages from proppages

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
   safelist: ['outline-none'],
   theme: {
     extend: {
