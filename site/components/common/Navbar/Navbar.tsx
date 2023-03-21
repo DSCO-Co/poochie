7e9457451ed3ae2a8967184030cdd99d619977d6
Jasper-Rowan <jasper.rowan@berkeley.edu>
2023-03-21 16:23:07 -0700

    :sparkles: Toggle search bar in navbar

--- site/components/common/Navbar/Navbar.tsx
+++ site/components/common/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Link from 'next/link'
 import s from './Navbar.module.css'
 import NavbarRoot from './NavbarRoot'
@@ -14,47 +14,73 @@ interface NavbarProps {
   links?: Link[]
 }
 
-const Navbar: FC<NavbarProps> = ({ links }) => (
-  <NavbarRoot>
-    <div className=" h-[44px] relative z-50">
-      <FreeShippingBanner />
-    </div>
-    <Container clean className="mx-auto max-w-8xl px-6">
-      <div className={s.nav}>
-        <div className="flex items-center flex-1">
-          <Link href="/" className={s.logo} aria-label="Logo">
-            <Logo />
-          </Link>
+const Navbar: FC<NavbarProps> = ({ links }) => {
+  const [showSearchBar, setShowSearchBar] = useState(false)
+
+  return (
+    <NavbarRoot>
+      <div className=" h-[44px] relative z-50">
+        <FreeShippingBanner />
+      </div>
+      <Container clean className="mx-auto max-w-8xl px-6">
+        <div className={s.nav}>
+          <div className="flex items-center flex-1">
+            <Link href="/" className={s.logo} aria-label="Logo">
+              <Logo />
+            </Link>
+          </div>
+          {/* Bottom row of links */}
+          <nav className={s.navMenu}>
+            <Link href="/search" className={s.link}>
+              All
+            </Link>
+            {links?.map((l) => (
+              <Link href={l.href} key={l.href} className={s.link}>
+                {l.label}
+              </Link>
+            ))}
+          </nav>
+          <div className="flex items-center justify-end flex-1 space-x-8">
+            {process.env.COMMERCE_SEARCH_ENABLED && (
+              <div className="flex justify-center items-center">
+                <button
+                  onClick={() =>
+                    setShowSearchBar((prevShowSearchBar) => !prevShowSearchBar)
+                  }
+                >
+                  <svg
+                    className="h-6 w-6"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            )}
+            <UserNav />
+          </div>
         </div>
         {process.env.COMMERCE_SEARCH_ENABLED && (
-          <div className="justify-center flex-1 hidden lg:flex">
-            <Searchbar />
+          <div className="flex pb-4 lg:px-6 lg:hidden">
+            <Searchbar id="mobile-search" />
           </div>
         )}
-        <div className="flex items-center justify-end flex-1 space-x-8">
-          <UserNav />
-        </div>
-      </div>
-      {process.env.COMMERCE_SEARCH_ENABLED && (
-        <div className="flex pb-4 lg:px-6 lg:hidden">
-          <Searchbar id="mobile-search" />
-        </div>
-      )}
-    </Container>
-    {/* Bottom row of links */}
-    <div className='flex flex-col justify-center'>
-      <nav className={s.navMenu}>
-        <Link href="/search" className={s.link}>
-          All
-        </Link>
-        {links?.map((l) => (
-          <Link href={l.href} key={l.href} className={s.link}>
-            {l.label}
-          </Link>
-        ))}
-      </nav>
-    </div>
-  </NavbarRoot>
-)
+        {/* Desktop search bar */}
+        {process.env.COMMERCE_SEARCH_ENABLED && (
+          <div className="flex justify-center mb-4">
+            <div className="justify-center w-[320px] hidden lg:flex">
+              {showSearchBar && <Searchbar />}
+            </div>
+          </div>
+        )}
+      </Container>
+    </NavbarRoot>
+  )
+}
 
 export default Navbar
