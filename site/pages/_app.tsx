dcaa1a4b229a8029cea12eb379e69616e99eed55
Callam Ingram <callam@berkeley.edu>
2023-04-06 16:24:52 -0700

    'fuck it its a private repo right?'

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -15,7 +15,9 @@ import { ManagedUIContext } from '@components/ui/context'
 
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
 
-
+const ALGOLIA_APP_ID = "ULGMM9NZ0C";
+const ALGOLIA_SEARCH_API_KEY = "9f9cfb8dddc404f45286e315c78b8127";
+const searchClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop
@@ -28,10 +30,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head />
       <ManagedUIContext>
+        <InstantSearch searchClient={searchClient} indexName="Products">
           <Configure />
           <Layout pageProps={pageProps}>
             <Component {...pageProps} />
           </Layout>
+        </InstantSearch>
       </ManagedUIContext>
     </>
   )
