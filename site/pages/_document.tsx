5b41cb7c9dc00c98deee49ae72b5cecfe99bba6b
Jasper-Rowan <jasper.rowan@berkeley.edu>
2023-04-11 15:43:53 -0700

    :construction: WIP - Manually added GTM scropts to _document.tsx and added custom domain that we are using in stape, not sure if this works yet only check out this commit if you

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -5,6 +5,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* GTM server container script with custom subdomain for stape.io container more info : https://morganfeeney.com/guides/how-to-integrate-google-tag-manager-with-nextjs */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://ss.poochie.co/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-XXXX');`,
+            }}
+          />
           {/* <link rel="icon" href="/bc_favicon.ico" /> */}
           <link
             rel="apple-touch-icon"
@@ -32,6 +42,11 @@ class MyDocument extends Document {
         <body className="loading">
           <Main />
           <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://ss.poochie.co/ns.html?id=GTM-XXXX" height="0" width="0" style="display: none; visibility: hidden;" />`,
+            }}
+          />
         </body>
       </Html>
     )
