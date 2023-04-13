d99f488e538111d8d6714d382a16a687b67f0f26
Jasper-Rowan <jasper.rowan@berkeley.edu>
2023-04-12 21:44:54 -0700

    :sparkles: Connected stape api route to GTM server container so it not recieves all url changes and forwards them to GTM

--- site/pages/api/webhooks/stape.ts
+++ site/pages/api/webhooks/stape.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 // localhost:3000/api/webhooks/stape
 
 /*
@@ -7,25 +9,44 @@
     server: POST https://ss.poochie.co/collect, whatever...
 */
 export default async function handler(req, res) {
-  const requestMethod = req.method
+  const requestMethod = req.method;
 
   switch (requestMethod) {
     case 'GET':
-      res.status(200).json({
-        message: `Get requests are not supported. Did you mean to make a POST request?`,
-      })
-      break
+      res
+        .status(200)
+        .json({
+          message: `Get requests are not supported. Did you mean to make a POST request?`,
+        });
+      break;
 
     case 'POST':
-      const eventData = req.body
-      console.log('Received event data:', eventData)
+      const eventData = req.body;
+      console.log('Received event data:', eventData);
 
-      res.status(200).json({ message: 'Hey it worked in stape routes' })
-      break // Add this break statement
+      const url = eventData.url ? encodeURIComponent(eventData.url) : '';
+
+      try {
+        const response = await axios.get(
+          `https://ss.poochie.co/g/collect?v=2&en=page_view&tid=G-1234&cid=123.456&dl=${url}`,
+          {
+            headers: {
+              'x-gtm-server-preview': 'ZW52LTR8Mm1kZnBEQ1ZpdHhJemRkbkNEaU9zd3wxODc3OGI0OWE0MjQ3OTA3ODJmMDQ=',
+            },
+          }
+        );
+
+        console.log('Axios response:', response.data);
+      } catch (error) {
+        console.error('Axios request failed:', error);
+      }
+
+      res.status(200).json({ message: 'Hey it worked in stape routes' });
+      break;
 
     // handle other HTTP methods
     default:
-      res.status(200).json({ message: 'Welcome to API Routes!' })
-      break // Add this break statement (optional)
+      res.status(200).json({ message: 'Welcome to API Routes!' });
+      break;
   }
 }
