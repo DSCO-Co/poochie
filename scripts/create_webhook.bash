30b72a4549b28a4e0ef5582a4f498e45e12477c2
Jasper-Rowan <jasper.rowan@berkeley.edu>
2023-04-25 15:25:32 -0700

    :sparkles: Added more scripts for listing, creating and deleting webhook subs to the big commerce store

--- scripts/create_webhook.bash
+++ scripts/create_webhook.bash
@@ -1,9 +1,20 @@
+#!/bin/bash
+
+destination="$1"
+
+if [ -z "$destination" ]; then
+  echo "Error: A destination URL is required."
+  echo "Usage: $0 <destination_url>"
+  echo "Example: $0 https://example.com/api/webhooks/bigcommerce"
+  exit 1
+fi
+
 curl -X POST "https://api.bigcommerce.com/stores/day26hsh2m/v3/hooks" \
 -H "X-Auth-Token: 3tforhhkp86u8bwc9zbt6s5ms89j3ob" \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
--d '{ 
- "scope": "store/cart/*", 
- "destination": "https://ea6a-47-156-232-192.ngrok-free.app/api/webhooks/bigcommerce", 
- "is_active": true 
-}' 
+-d '{
+  "scope": "store/cart/*",
+  "destination": "'"$destination"'",
+  "is_active": true
+}'
