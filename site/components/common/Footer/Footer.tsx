f48f6dd2eb8436cebdbb3f5c845cc82f8d682a39
William Kasel <wkasel@gmail.com>
2023-04-25 19:17:01 -0700

    data

--- site/components/common/Footer/Footer.tsx
+++ site/components/common/Footer/Footer.tsx
@@ -42,11 +42,11 @@ const navigation = {
     { name: 'API Status', href: '#' },
   ],
   company: [
-    { name: 'About Us', href: '/about-us'},
-    { name: 'Refund Policy', href: '/policies/refund-policy'},
-    { name: 'Privacy Policy', href: '/policies/privacy-policy'},
-    { name: 'Terms of Service', href:'/policies/terms-of-service'},
-    { name: 'Shipping & Tracking', href: '/shipping'},
+    { name: 'About Us', href: '/about-us' },
+    { name: 'Refund Policy', href: '/policies/refund-policy' },
+    { name: 'Privacy Policy', href: '/policies/privacy-policy' },
+    { name: 'Terms of Service', href: '/policies/terms-of-service' },
+    { name: 'Shipping & Tracking', href: '/shipping' },
   ],
   legal: [
     { name: 'Claim', href: '#' },
@@ -347,7 +347,7 @@ const navigation = {
   social: [
     {
       name: 'Facebook',
-      href: '#',
+      href: 'https://www.facebook.com/shoppoochie.co',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -360,7 +360,7 @@ const navigation = {
     },
     {
       name: 'Instagram',
-      href: '#',
+      href: 'https://instagram.com/shop_poochie',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -373,7 +373,7 @@ const navigation = {
     },
     {
       name: 'Twitter',
-      href: '#',
+      href: 'https://twitter.com/shoppoochie',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -381,31 +381,27 @@ const navigation = {
       ),
     },
     {
-      name: 'GitHub',
+      name: 'TikTok',
       href: '#',
       icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-            clipRule="evenodd"
-          />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2859 3333" fill="currentColor" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" {...props}>
+          <path d="M2081 0c55 473 319 755 778 785v532c-266 26-499-61-770-225v995c0 1264-1378 1659-1932 753-356-583-138-1606 1004-1647v561c-87 14-180 36-265 65-254 86-398 247-358 531 77 544 1075 705 992-358V1h551z" />
         </svg>
       ),
     },
-    {
-      name: 'YouTube',
-      href: '#',
-      icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
+    // {
+    //   name: 'YouTube',
+    //   href: '#',
+    //   icon: (props) => (
+    //     <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+    //       <path
+    //         fillRule="evenodd"
+    //         d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
+    //         clipRule="evenodd"
+    //       />
+    //     </svg>
+    //   ),
+    // },
   ],
 }
 
@@ -432,13 +428,13 @@ const Footer: FC<Props> = ({ className, pages, categories }) => {
                 <div key={page.id}>
                   <ul role="list" className="mt-6 space-y-4">
                     <li key={page.id}>
-                      <a
+                      <Link
                         href={`/${page.slug}`}
                         className="text-sm leading-6 text-gray-400 hover:text-gray-200"
                       >
                         {' '}
                         {page.name}{' '}
-                      </a>
+                      </Link>
                     </li>
                   </ul>
                 </div>
@@ -454,13 +450,13 @@ const Footer: FC<Props> = ({ className, pages, categories }) => {
               <div key={page.name}>
                 <ul role="list" className="mt-6 space-y-4">
                   <li key={page.name}>
-                    <a
+                    <Link
                       href={`/${getSlug(page.href)}`}
                       className="text-sm leading-6 text-gray-400 hover:text-gray-200"
                     >
                       {' '}
                       {page.name}{' '}
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </div>
@@ -476,13 +472,13 @@ const Footer: FC<Props> = ({ className, pages, categories }) => {
                 <div key={page.name}>
                   <ul role="list" className="mt-6 space-y-4">
                     <li>
-                      <a
-                        href={`/${getSlug(page.href)}`}
+                      <Link
+                        href={`${getSlug(page.href)}`}
                         className="text-sm leading-6 text-gray-400 hover:text-gray-100"
                       >
                         {' '}
                         {page.name}{' '}
-                      </a>
+                      </Link>
                     </li>
                   </ul>
                 </div>
@@ -526,35 +522,36 @@ const Footer: FC<Props> = ({ className, pages, categories }) => {
 
         <div className="pt-8 mt-8 border-t border-gray-900/10 md:flex md:items-center md:justify-between">
           {/* Copyright */}
-          <div className="text-base text-gray-300 mt-8 md:mt-0 md:order-1">
+          <div className="mt-8 text-base text-gray-300 md:mt-0 md:order-1">
             &copy; 2023 Poochie Co. All rights reserved.
           </div>
 
           {/* Payment items */}
-          <div className="flex flex-wrap space-x-6 space-y-0 mt-8  md:order-2">
+          <div className="flex flex-wrap mt-8 space-x-6 space-y-0 md:order-2">
             {navigation.paymentMethods.map((item) => (
-              <a
+              <Link
                 key={item.name}
                 href={item.href}
                 className="text-gray-300 hover:text-gray-500"
               >
                 <span className="sr-only">{item.name}</span>
                 <item.icon className="w-6 h-6" aria-hidden="true" />
-              </a>
+              </Link>
             ))}
           </div>
 
           {/* Social items */}
-          <div className="flex space-x-6 mt-8 md:mt-0 md:order-3">
+          <div className="flex mt-8 space-x-6 md:mt-0 md:order-3">
             {navigation.social.map((item) => (
-              <a
+              <Link
                 key={item.name}
                 href={item.href}
                 className="text-gray-300 hover:text-gray-500"
               >
+
                 <span className="sr-only">{item.name}</span>
-                <item.icon className="w-6 h-6" aria-hidden="true" />
-              </a>
+                <item.icon className="w-6 h-6" />
+              </Link>
             ))}
           </div>
         </div>
