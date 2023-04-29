c81639c0e06b10219f26d25b7641d62c0b7bbe68
William Kasel <wkasel@gmail.com>
2023-04-29 03:04:48 -0700

    Janky ass clearance button

--- site/components/common/Banners/FreeShippingBanner.tsx
+++ site/components/common/Banners/FreeShippingBanner.tsx
@@ -14,7 +14,7 @@ export function FreeShippingBanner() {
         <div className="h-[44px] w-screen relative flex items-center gap-x-6 bg-pink-super py-2.5 px-6 before:flex-1">
           <p className="text-sm leading-6 text-white">
             <strong className="font-bold">
-              Free Shipping on Orders over $100
+              FAST SHIPPING GUARANTEED! FREE SHIPPING ON ORDERS OVER $100
             </strong>
           </p>
           <div className="flex justify-end flex-1">
