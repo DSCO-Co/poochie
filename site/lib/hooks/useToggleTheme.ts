6a87efd9190e8d1de9e956cf5493158918fe9dff
William Kasel <wkasel@gmail.com>
2023-05-03 21:32:57 -0700

    texting Segment side by side with GTA for conversions

--- site/lib/hooks/useToggleTheme.ts
+++ site/lib/hooks/useToggleTheme.ts
@@ -5,6 +5,7 @@ export const useToggleTheme = () => {
   const { theme, themes, setTheme } = useTheme()
   const [themeValue, setThemeValue] = useState<string>('system')
 
+  // @ts-ignore
   useEffect(() => setThemeValue(theme), [theme])
 
   return { theme: themeValue, setTheme, themes }
--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -6,14 +6,15 @@ import algoliasearch from 'algoliasearch/lite'
 import { Configure, InstantSearch } from 'react-instantsearch-hooks-web'
 
 // import { pageViewed } from '@lib/Segment/segmentAnalytics'
-import { SegmentComponent } from '@components/SegmentComponent'
+// import { SegmentComponent } from '@components/SegmentComponent'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
 import { CookieProvider } from '@lib/contexts'
+import * as snippet from '@segment/snippet'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 import { FC, ReactNode, useEffect } from 'react'
-
 
 import * as gtag from '@lib/gtag'
 import { Analytics } from '@vercel/analytics/react'
@@ -33,10 +34,27 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop;
   const router = useRouter();
 
+  function renderSnippet() {
+    const opts = {
+      apiKey: process.env.NEXT_PUBLIC_ANALYTICS_WRITE_KEY || WRITE_KEY,
+      // note: the page option only covers SSR tracking.
+      // Page.js is used to track other events using `window.analytics.page()`
+      page: true,
+    }
 
+    if (process.env.NODE_ENV === 'development') {
+      return snippet.max(opts)
+    }
+
+    return snippet.min(opts)
+  }
 
   useEffect(() => {
     document.body.classList?.remove('loading')
+    if (window !== undefined) {
+      // @ts-ignore
+      window.analytics.page()
+    }
   }, [])
 
   useEffect(() => {
@@ -54,13 +72,16 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head />
-
+      <Script
+        id="segment-script"
+        dangerouslySetInnerHTML={{ __html: renderSnippet() }}
+      />
       <CookieProvider>
         <ManagedUIContext>
           <InstantSearch searchClient={searchClient} indexName="Products">
             <Configure />
             <Layout pageProps={pageProps}>
-              <SegmentComponent writeKey={WRITE_KEY} />
+
               <Component {...pageProps} />
               <Analytics />
             </Layout>
