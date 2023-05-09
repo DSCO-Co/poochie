a3abebc424b7c6906482993dc3eb339907a1c878
William Kasel <wkasel@gmail.com>
2023-05-09 09:08:49 -0700

    Update tag

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -15,10 +15,12 @@ const MyDocument = () => {
           src="https://static.zdassets.com/ekr/snippet.js?key=5526ab38-c934-45d0-8bce-8fce9c7738fe"
           strategy="beforeInteractive"
         />
+
+        {/* Cometly Tag */}
         <meta name="cometly-domain-verification" content="89ff378f-6d84-4756-9948-f401f111735c" />
         <Script
           id="cometlytrack"
-          src="https://t.cometlytrack.com/e?uid=5f4c9f-3377699720527922-e8cace-s"
+          src="https://t.cometlytrack.com/e?uid=caf5e9-4503599627370554-b15e9c-s"
         />
         <Script
           id="cometlytrack-init"
